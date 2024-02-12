80cd230fab0259fc220a7fbb5a2effe7e7c9ac34
“ALVINdimpos” <alvindimpos@gmail.com>
2024-02-12 12:58:34 +0200

    ft: Update cards

--- src/containers/Category.jsx
+++ src/containers/Category.jsx
@@ -1,63 +1,266 @@
-const categorySection = () => {
+import Slider from "react-slick";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const categories = [
+  {
+    title: "4Runner",
+    imageUrl:
+      "https://www.toyota.com/content/dam/toyota/jellies/transitional/2024/4runner/10.png",
+  },
+
+  {
+    title: "Celica",
+    imageUrl:
+      "https://edgecast-img.yahoo.net/mysterio/api/84257A485F27F0F11E70E96441F02569C2FC8039E7DA404B74368DDDBD35015A/autoblog/resizefill_w788_h525;quality_80;format_webp;cc_31536000;/https://s.aolcdn.com/commerce/autodata/images/USB50TOC031A0101.jpg",
+  },
+  {
+    title: "Corolla",
+    imageUrl:
+      "https://www.motortrend.com/uploads/sites/10/2021/09/2022-toyota-corolla-hybrid-le-sedan-angular-front.png?fit=around%7C875:492",
+  },
+  {
+    title: "Corolla Cross",
+    imageUrl:
+      "https://www.toyotarwanda.com/media/gamme/modeles/images/c17c68f3853160d844721a9676dc29e9.png",
+  },
+  {
+    title: "Corolla iM",
+    imageUrl:
+      "https://www.motortrend.com/uploads/sites/10/2017/09/2018-toyota-corolla-im-hatchback-angular-front.png?fit=around%7C875:492",
+  },
+  {
+    title: "Cressida",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod/images/1981-toyota-cressida-104-6441661cc24d5.jpg?crop=0.708xw:0.527xh;0.188xw,0.348xh&resize=1200:*",
+  },
+  {
+    title: "Echo",
+    imageUrl:
+      "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcR-OEI8FO1owZUz4C0Hv33e-eVhSVW5BHrVX6ZmIudhLhDBqoEe",
+  },
+  {
+    title: "FJ Cruiser",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/a/af/2011_Toyota_FJ_Cruiser_%28GSJ15R%29_wagon_%282011-11-08%29_01.jpg",
+  },
+  {
+    title: "GR Supra",
+    imageUrl:
+      "https://cdn.motor1.com/images/mgl/0eeJ2W/s1/2023-toyota-supra-with-manual-gearbox-europe.webp",
+  },
+  {
+    title: "GR86",
+    imageUrl:
+      "https://86speed.com/images/thumbs/w_6_0022497_artisan-spirits-sports-line-black-label-gt-full-widebody-2022-toyota-gr86.jpeg",
+  },
+  {
+    title: "Highlander",
+    imageUrl:
+      "https://media.ed.edmunds-media.com/toyota/grand-highlander/2024/oem/2024_toyota_grand-highlander_4dr-suv_platinum_fq_oem_1_815.jpg",
+  },
+  {
+    title: "Land Cruiser",
+    imageUrl:
+      "https://imgd.aeplcdn.com/664x374/n/cw/ec/139739/land-cruiser-exterior-right-front-three-quarter-2.jpeg?isig=0&q=80",
+  },
+  {
+    title: "Matrix",
+    imageUrl:
+      "https://platform.cstatic-images.com/xlarge/in/v2/stock_photos/d48b16f0-4fdb-4cb3-812a-9fec8dbfdf3f/1b17c8af-1497-4bfb-8701-7a0caddfc2d7.png",
+  },
+  {
+    title: "Mirai",
+    imageUrl:
+      "https://stimg.cardekho.com/images/carexteriorimages/630x420/Toyota/Mirai/2421/1648822281719/front-left-side-47.jpg?tr=w-664",
+  },
+  {
+    title: "MR2",
+    imageUrl:
+      "https://www.motortrend.com/uploads/2023/07/Toyota-Sports-EV-Concept.jpg?fit=around%7C875:492",
+  },
+  {
+    title: "MR2 Spyder",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod/images/2003-mr2-spyder-2-1599224809.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*",
+  },
+  {
+    title: "Paseo",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/1/1a/Toyota_Paseo_--_09-07-2009.jpg",
+  },
+  {
+    title: "Pickup",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/0/02/Ford_F-150_crew_cab_--_05-28-2011.jpg",
+  },
+  {
+    title: "Previa",
+    imageUrl:
+      "https://ymimg1.b8cdn.com/resized/car_model/2987/pictures/2782376/webp_listing_main_thumb.webp",
+  },
+  {
+    title: "Prius",
+    imageUrl:
+      "https://www.freep.com/gcdn/presto/2023/04/03/PDTF/3f656094-eea6-4739-a74f-55bce1990759-IMG_5353.JPEG?width=1320&height=990&fit=crop&format=pjpg&auto=webp",
+  },
+  {
+    title: "Prius AWD-e",
+    imageUrl:
+      "https://www.motortrend.com/uploads/sites/5/2019/04/2019-Toyota-Prius-AWD-e-front-side-view-parked.jpg?fit=around%7C875:492",
+  },
+  {
+    title: "Prius C",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/b/b8/2012_Toyota_Prius_c_%28NHP10R%29_hatchback_%282015-07-03%29_01.jpg",
+  },
+  {
+    title: "Prius Plug-In",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/5/54/2019_Toyota_Prius_Business_Edition%2B_PHEV_1.8.jpg",
+  },
+  {
+    title: "Prius Prime",
+    imageUrl:
+      "https://hips.hearstapps.com/hmg-prod/images/2023-toyota-prius-prime-xse-474-647f7e6f0b72e.jpg?crop=0.606xw:0.511xh;0.335xw,0.465xh&resize=1200:*",
+  },
+  {
+    title: "Prius V",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/0/02/2012_Toyota_Prius_v_--_03-21-2012.JPG",
+  },
+  {
+    title: "RAV4",
+    imageUrl: "https://trudelauto.com/en/image/rav4",
+  },
+  {
+    title: "RAV4 Prime",
+    imageUrl: "https://trudelauto.com/en/image/rav4-prime",
+  },
+  {
+    title: "Sequoia",
+    imageUrl: "https://trudelauto.com/en/image/sequoia",
+  },
+  {
+    title: "Sienna",
+    imageUrl: "https://trudelauto.com/en/image/sienna",
+  },
+  {
+    title: "Solara",
+    imageUrl: "https://trudelauto.com/en/image/solara",
+  },
+  {
+    title: "Starlet",
+    imageUrl: "https://trudelauto.com/en/image/starlet",
+  },
+  {
+    title: "Supra",
+    imageUrl: "https://trudelauto.com/en/image/supra",
+  },
+  {
+    title: "T100",
+    imageUrl: "https://trudelauto.com/en/image/t100",
+  },
+  {
+    title: "Tacoma",
+    imageUrl: "https://trudelauto.com/en/image/tacoma",
+  },
+  {
+    title: "Tercel",
+    imageUrl: "https://trudelauto.com/en/image/tercel",
+  },
+  {
+    title: "Tundra",
+    imageUrl: "https://trudelauto.com/en/image/tundra",
+  },
+  {
+    title: "Van",
+    imageUrl: "https://trudelauto.com/en/image/van",
+  },
+  {
+    title: "Venza",
+    imageUrl: "https://trudelauto.com/en/image/venza",
+  },
+  {
+    title: "Yaris",
+    imageUrl: "https://trudelauto.com/en/image/yaris",
+  },
+  {
+    title: "Yaris iA",
+    imageUrl: "https://trudelauto.com/en/image/yaris-ia",
+  },
+  {
+    title: "All Toyota Models",
+    imageUrl: "https://trudelauto.com/en/image/all-toyota-models",
+  },
+];
+const CategorySection = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    arrows: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
-    <div className="max-w-screen-xl  px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 ">
-      <h1 className="text-3xl font-semibold mb-4">Shop By Category</h1>
-      <p className="text-gray-600 mb-8">
+    <div className="max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+      <h1 className="mb-4 text-3xl font-semibold">Shop By Category</h1>
+      <p className="mb-8 text-gray-600">
         Sagittis Purus Sit Amet Ultrices Dui Volutpat Consequat Mauris.
       </p>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-        <div className="flex flex-col   rounded-xl dark:bg-white dark:border-bg-black ">
-          <img
-            className="w-full h-auto rounded-t-xl"
-            src="https://trudelauto.com/en/image/1181755"
-            alt="Image Description"
-          />
-          <div className="p-4 md:p-5">
-            <h3 className="text-lg font-bold text-black dark:text-black">
-              Card title
-            </h3>
+      <Slider {...settings} className="mb-8">
+        {categories.map((category, index) => (
+          <div key={index}>
+            <div
+              key={index}
+              className="flex flex-col p-4 mr-4 rounded-xl dark:bg-white dark:border-bg-black"
+            >
+              <img
+                className="w-full h-auto mb-4 rounded-t-xl"
+                src={category.imageUrl}
+                alt="Image Description"
+              />
+              <div>
+                <h3 className="text-lg font-bold text-black dark:text-black">
+                  {category.title}
+                </h3>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col   rounded-xl dark:bg-white dark:border-bg-black ">
-          <img
-            className="w-full h-auto rounded-t-xl"
-            src="https://trudelauto.com/en/image/1181755"
-            alt="Image Description"
-          />
-          <div className="p-4 md:p-5">
-            <h3 className="text-lg font-bold text-black dark:text-black">
-              Card title
-            </h3>
-          </div>
-        </div>
-        <div className="flex flex-col   rounded-xl dark:bg-white dark:border-bg-black ">
-          <img
-            className="w-full h-auto rounded-t-xl"
-            src="https://trudelauto.com/en/image/1181755"
-            alt="Image Description"
-          />
-          <div className="p-4 md:p-5">
-            <h3 className="text-lg font-bold text-black dark:text-black">
-              Card title
-            </h3>
-          </div>
-        </div>
-        <div className="flex flex-col   rounded-xl dark:bg-white dark:border-bg-black ">
-          <img
-            className="w-full h-auto rounded-t-xl"
-            src="https://trudelauto.com/en/image/1181755"
-            alt="Image Description"
-          />
-          <div className="p-4 md:p-5">
-            <h3 className="text-lg font-bold text-black dark:text-black">
-              Card title
-            </h3>
-          </div>
-        </div>
-      </div>
+        ))}
+      </Slider>
     </div>
   );
 };
 
-export default categorySection;
+export default CategorySection;
--- src/containers/Hero.jsx
+++ src/containers/Hero.jsx
@@ -1,8 +1,117 @@
-import { Link } from "react-router-dom";
+import { useState } from "react";
 import { heroPic } from "../assets";
+
 const HeroSection = () => {
+  const [activeTab, setActiveTab] = useState("shopByVehicle");
+
+  const renderTabContent = () => {
+    switch (activeTab) {
+      case "shopByVehicle":
+        return (
+          <div className="relative flex gap-2 p-2 bg-white rounded-md shadow-sm h-30">
+            <div className="flex items-center px-4 py-2 border border-gray-300 rounded-md appearance-none focus:outline-none focus:border-blue-500">
+              <label htmlFor="year" className="mr-2">
+                Year:
+              </label>
+              <select id="year" className="">
+                <option selected></option>
+                <option value="2020">2020</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+                <option value="2021">2021</option>
+              </select>
+            </div>
+
+            <div className="flex items-center px-4 py-2 border border-gray-300 rounded-md appearance-none focus:outline-none focus:border-blue-500">
+              <label>Make:</label>
+              <select className="input-dropdown">
+                <option selected></option>
+                <option value="Toyota">Toyota</option>
+                <option value="Honda">Honda</option>
+                <option value="Toyota">Toyota</option>
+                <option value="Honda">Honda</option>
+                <option value="Toyota">Toyota</option>
+                <option value="Honda">Honda</option>
+              </select>
+            </div>
+
+            <div className="flex items-center px-4 py-2 border border-gray-300 rounded-md appearance-none focus:outline-none focus:border-blue-500">
+              <label>Model:</label>
+              <select className="input-dropdown">
+                <option selected></option>
+                <option value="Corolla">Corolla</option>
+                <option value="Civic">Civic</option>
+                <option value="Corolla">Corolla</option>
+                <option value="Civic">Civic</option>
+              </select>
+            </div>
+
+            <div className="flex items-center px-4 py-2 border border-gray-300 rounded-md appearance-none focus:outline-none focus:border-blue-500">
+              <label>Trim:</label>
+              <select className="input-dropdown">
+                <option selected></option>
+                <option value="LE">LE</option>
+                <option value="SE">SE</option>
+              </select>
+            </div>
+
+            <div className="flex items-center px-4 py-2 border border-gray-300 rounded-md appearance-none focus:outline-none focus:border-blue-500">
+              <label>Engine:</label>
+              <select className="input-dropdown">
+                <option selected></option>
+                <option value="1.8L">1.8L</option>
+                <option value="2.0L">2.0L</option>
+                <option selected></option>
+              </select>
+            </div>
+          </div>
+        );
+      case "searchByPart":
+        return (
+          <div className="relative flex gap-2 p-2 bg-white rounded-md shadow-sm h-30">
+            <div className="flex justify-between w-full px-4 py-2 border border-gray-300 rounded-md appearance-none focus:outline-none focus:border-blue-500">
+              <label className="m-auto text-sm font-medium text-left text-gray-700 ">
+                Part Number:
+              </label>
+              <input
+                type="text"
+                className="w-2/3 px-2 py-1 mr-2 border border-gray-300 rounded-md appearance-none  focus:outline-none focus:border-blue-500"
+              />
+              <button className="px-4 text-white transition duration-200 ease-in-out bg-red-600 rounded-sm ">
+                Find Part
+              </button>
+            </div>
+          </div>
+        );
+      case "searchByVin":
+        return (
+          //
+          <div className="relative flex p-2 bg-white rounded-md shadow-sm h-30">
+            <div className="flex w-2/3 gap-1 px-4 py-2 border border-gray-300 rounded-md appearance-none focus:outline-none focus:border-blue-500">
+              <label className="m-auto text-sm font-medium text-left text-gray-700 ">
+                Vin:
+              </label>
+              <input
+                type="text"
+                className="w-2/3 px-2 py-1 border border-gray-300 rounded-md appearance-none  focus:outline-none focus:border-blue-500"
+              />
+              <button className="px-4 text-white transition duration-200 ease-in-out bg-red-600 rounded-sm ">
+                Find Part
+              </button>
+            </div>
+          </div>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
-    <section className="bg-white dark:bg-white">
+    <section className="bg-white mt-[-30px] dark:bg-white">
       <div className="grid max-w-screen-xl h-[100vh] px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
           <h1 className="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-black">
@@ -12,30 +121,29 @@ const HeroSection = () => {
             Duis bibendum rhoncus felis, sit amet pretium libero ornare ut.
             Pellentesque a ullamcorper enim.
           </p>
-          <Link
-            to="/"
-            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
-          >
-            Get started
-            <svg
-              className="w-5 h-5 ml-2 -mr-1"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
+
+          <div className="flex mb-4 space-x-4">
+            <button
+              className={`tab nav-link bg-red-600 text-white rounded-sm transition duration-200 ease-in-out px-4 py-2 uppercase text-center font-semibold hover:bg-red-800 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200${activeTab === "shopByVehicle" ? "active" : ""}`}
+              onClick={() => setActiveTab("shopByVehicle")}
             >
-              <path
-                fillRule="evenodd"
-                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </Link>
-          <Link
-            to="/contact-us"
-            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center bg-red-500 rounded-lg text-while focus:ring-4 focus:ring-red-100 dark:text-white dark:border-red-700 dark:hover:bg-red-700 dark:focus:ring-red-800"
-          >
-            customer request
-          </Link>
+              SHOP BY VEHICLE
+            </button>
+            <button
+              className={`tab nav-link bg-red-600 text-white rounded-sm transition duration-200 ease-in-out px-4 py-2 uppercase text-center font-semibold hover:bg-red-800 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 ${activeTab === "searchByPart" ? "active" : ""}`}
+              onClick={() => setActiveTab("searchByPart")}
+            >
+              SEARCH BY PART
+            </button>
+            <button
+              className={`tab nav-link bg-red-600 text-white rounded-sm transition duration-200 ease-in-out px-4 py-2 uppercase text-center font-semibold hover:bg-red-800 focus:outline-none focus:border-red-700 focus:ring focus:ring-red-200 ${activeTab === "searchByVin" ? "active" : ""}`}
+              onClick={() => setActiveTab("searchByVin")}
+            >
+              SEARCH BY VIN
+            </button>
+          </div>
+
+          <div className="tab-content">{renderTabContent()}</div>
         </div>
         <div className="hidden lg:mt-0 lg:col-span-5 lg:flex w-[100%] h-[100%]">
           <img src={heroPic} alt="mockup" />
