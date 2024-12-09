09845cb837f90123e7754281b29c6e9212ef7f16
kagaba-etienne <kagabaetienne04@gmail.com>
2024-12-09 20:21:14 +0200

    fx(battery): add delete battery feature

--- backend/app/api/v2/battery/__init__.py
+++ backend/app/api/v2/battery/__init__.py
@@ -1,4 +1,4 @@
-from . import add, get, update
+from . import add, get, update, delete
 from fastapi import APIRouter, Depends
 
 router = APIRouter()
@@ -6,3 +6,4 @@ router = APIRouter()
 router.include_router(add.router)
 router.include_router(get.router)
 router.include_router(update.router)
+router.include_router(delete.router)
